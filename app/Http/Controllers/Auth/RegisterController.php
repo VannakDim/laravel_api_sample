78cd252423505118792614b6b3c1432ffccfa12d
M1 <studio@M1s-Mac-mini.local>
2026-04-03 14:24:33 +0700

    Fix the RegisterController with RegisterRequest

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,8 +14,9 @@ class RegisterController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(RegisterRequest $request)
     {
+
 
         $user = User::create($request->only('name', 'email') + [
             'password' => Hash::make($request->password)
